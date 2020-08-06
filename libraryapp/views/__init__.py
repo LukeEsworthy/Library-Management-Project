704da4785074a55968003dfd576f1ebb816405e5
Luke Esworthy <lukesworthy@gmail.com>
2020-08-06 13:43:03 -0500

    chapter 5 complete, added form functionality for adding books and libraries

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,5 +1,7 @@
 from .books.list import book_list
 from .librarians.list import librarian_list
 from .libraries.list import libraries_list
+from .libraries.form import library_form
 from .home import home
 from .auth.logout import logout_user
+from .books.form import book_form
